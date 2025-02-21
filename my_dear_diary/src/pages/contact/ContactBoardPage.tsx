8c60b6a309d66d37764b3f98cffe2f3d1427b8af
rosy94 <mya315748@gmail.com>
2025-02-21 16:57:13 +0900

    프로젝트명 변경

--- my_dear_diary/src/pages/contact/ContactBoardPage.tsx
+++
@@ -1,7 +0,0 @@
-// 연락처 페이지
-
-const ContactBoardPage = () => {
-  return <h1>📞 Contact</h1>;
-};
-
-export default ContactBoardPage;
--- my_dear_diary/src/pages/dashboard/DashboardPage.tsx
+++
@@ -1,7 +0,0 @@
-// 대시보드 페이지
-
-const DashboardPage = () => {
-  return <h1>📊 감정 차트</h1>;
-};
-
-export default DashboardPage;
