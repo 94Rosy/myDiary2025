76bb0bc09a8d247d3135c734b65b1dac29d14522
rosy94 <mya315748@gmail.com>
2025-04-07 19:39:02 +0900

    [page1] 메인 페이지 접근 권한

--- Diary/src/pages/main/MainPage.tsx
+++ Diary/src/pages/main/MainPage.tsx
@@ -1,14 +1,28 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { resetPage } from "../../store/paginationSlice";
 import MainCardList from "./addon/MainCardList";
+import { useNavigate } from "react-router-dom";
+import { RootState } from "../../store/store";
 import "./mainPage.scss";
 
 const MainPage = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state: RootState) => state.auth.user);
+
   useEffect(() => {
     dispatch(resetPage()); // 메인 진입 시 감정 일기의 페이지네이션 초기화
   }, [dispatch]);
+
+  const protectList = (path: string) => {
+    if (!user) {
+      alert("로그인이 필요해요!");
+      navigate("/");
+    } else {
+      navigate(path);
+    }
+  };
 
   return (
     <div className="main-wrapper">
@@ -20,15 +34,15 @@ const MainPage = () => {
       <div className="main-content">
         <MainCardList
           cardType="emotions"
-          link="/emotions"
           mainText="My Moods"
           subText="일기 기록하러 가기"
+          onClick={() => protectList("/emotions")}
         />
         <MainCardList
           cardType="dashboard"
-          link="/dashboard"
           mainText="Mood Trends"
           subText="기록 차트 보러 가기"
+          onClick={() => protectList("/dashboard")}
         />
       </div>
     </div>
