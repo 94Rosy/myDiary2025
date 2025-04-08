07a0fc284af15d7febc8ba6cf33c1ccbb101007e
rosy94 <mya315748@gmail.com>
2025-04-08 23:46:53 +0900

    페이지 진입 시 감정 데이터 패치할 수 있도록 수정

--- Diary/src/App.tsx
+++ Diary/src/App.tsx
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
 import AppRouter from "./routes/AppRouter";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "./store/store";
+import { fetchEmotions } from "./store/emotionSlice";
 import "./styles/global.scss";
 
 function App() {
+  const dispatch = useDispatch<AppDispatch>();
+  const emotions = useSelector((state: RootState) => state.emotions.emotions);
+
+  useEffect(() => {
+    if (!emotions.length) {
+      dispatch(fetchEmotions());
+    }
+  }, [dispatch, emotions.length]);
+
   return <AppRouter />;
 }
 
